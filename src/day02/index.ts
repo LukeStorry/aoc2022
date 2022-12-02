8b7ce388898c9a6c3890fcac3ef9909862616cc6
Luke Storry <luke.storry@letsdothis.com>
2022-12-02 10:54:35 +0000

    tsc part2 using mods

--- src/day02/index.ts
+++ src/day02/index.ts
@@ -7,20 +7,11 @@ const part1 = (rawInput: string): number =>
     return mine + [3, 0, 6][(theirs - mine + 3) % 3]
   }))
 
-const part2_results = new Map([
-  ["A X", 0 + 3],  // rock / lose (scissors)
-  ["A Y", 3 + 1],  // rock / draw (rock)
-  ["A Z", 6 + 2],  // rock / win (paper)
-  ["B X", 0 + 1],  // paper / lose (rock)
-  ["B Y", 3 + 2],  // paper / draw (paper)
-  ["B Z", 6 + 3],  // paper / win (scissors)
-  ["C X", 0 + 2],  // scissors / lose (paper)
-  ["C Y", 3 + 3],  // scissors / draw (scissors)
-  ["C Z", 6 + 1],  // scissors / win (rock)
-])
-
 const part2 = (rawInput: string): number =>
-  _.sum(rawInput.split(/\n/).map((g) => part2_results.get(g)))
+  _.sum(rawInput.split(/\n/).map((g) => {
+    const theirs = g.charCodeAt(0) - 64, ldw = g.charCodeAt(2) - 87
+    return 1 + (ldw + theirs) % 3 + [0, 3, 6][ldw - 1]
+  }))
 
 run({
   part1: { solution: part1, tests: [{ input: "A Y\nB X\nC Z", expected: 15, },], },
