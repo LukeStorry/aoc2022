5f5fd63fcc7e21bc416c4327ed96b2ab77423f3c
Luke Storry <luke.storry@letsdothis.com>
2022-12-02 14:50:42 +0000

    clean tsc

--- src/day02/index.ts
+++ src/day02/index.ts
@@ -3,19 +3,22 @@ import _ from "lodash";
 
 const part1 = (rawInput: string): number =>
   _.sum(rawInput.split(/\n/).map((g) => {
-    const theirs = g.charCodeAt(0) - 64, mine = g.charCodeAt(2) - 87
-    return mine + [3, 0, 6][(theirs - mine + 3) % 3]
+    const theirs = g.charCodeAt(0) - 'A'.charCodeAt(0)
+    const mine = g.charCodeAt(2) - 'X'.charCodeAt(0)
+    const drawLoseWin = (theirs - mine + 3) % 3;
+    return 1 + mine + [3, 0, 6][drawLoseWin]
   }))
 
 const part2 = (rawInput: string): number =>
   _.sum(rawInput.split(/\n/).map((g) => {
-    const theirs = g.charCodeAt(0) - 64, ldw = g.charCodeAt(2) - 87
-    return 1 + (ldw + theirs) % 3 + [0, 3, 6][ldw - 1]
+    const theirs = g.charCodeAt(0) - 'A'.charCodeAt(0)
+    const loseDrawWin = g.charCodeAt(2) - 'X'.charCodeAt(0)
+    return 1 + (loseDrawWin + theirs + 2) % 3 + [0, 3, 6][loseDrawWin]
   }))
 
 run({
-  part1: { solution: part1, tests: [{ input: "A Y\nB X\nC Z", expected: 15, },], },
-  part2: { solution: part2, tests: [{ input: "A Y\nB X\nC Z", expected: 12, },], },
+  part1: { solution: part1, tests: [{ input: "A Y\nB X\nC Z", expected: 15 }] },
+  part2: { solution: part2, tests: [{ input: "A Y\nB X\nC Z", expected: 12 }] },
   trimTestInputs: true,
   // onlyTests: true,
 });
