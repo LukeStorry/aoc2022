fdae22d6831cdd7f0194d57f15332b500e16c8cd
Luke Storry <luke.storry@letsdothis.com>
2022-12-05 23:18:51 +0000

    more rust typy stuff

--- src/day05/mod.rs
+++ src/day05/mod.rs
@@ -1,59 +1,87 @@
+use std::str::FromStr;
+
 use aoc_runner_derive::{aoc, aoc_generator};
 use itertools::Itertools;
 use regex::Regex;
 
 #[derive(Debug)]
-struct Move {
-    amount: usize,
-    from: usize,
-    to: usize,
-}
-type Stack = Vec<Vec<char>>;
+struct Move(usize, usize, usize);
+#[derive(Debug)]
+struct Stack(Vec<char>);
+#[derive(Debug)]
+struct Stacks(Vec<Stack>);
 
-fn parse_stack(s: &str) -> Stack {
-    let lines = s.lines().collect_vec();
-    lines
-        .last()
-        .unwrap()
-        .chars()
-        .enumerate()
-        .filter(|(_, c)| c.is_numeric())
-        .map(|(stack_index, c)| {
-            lines
-                .iter()
-                .map(|line| line.chars().nth(stack_index).unwrap())
-                .filter(|char| char.is_alphabetic())
-                .collect()
-        })
-        .collect()
+impl FromIterator<Stack> for Stacks {
+    fn from_iter<T: IntoIterator<Item = Stack>>(iter: T) -> Self {
+        Self(iter.into_iter().collect())
+    }
+}
+
+impl FromIterator<char> for Stack {
+    fn from_iter<T: IntoIterator<Item = char>>(iter: T) -> Self {
+        Self(iter.into_iter().collect())
+    }
+}
+
+impl FromStr for Stacks {
+    type Err = std::num::ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let lines = s.lines().collect_vec();
+        let vec = lines
+            .last()
+            .unwrap()
+            .chars()
+            .enumerate()
+            .filter(|(_, c)| c.is_numeric())
+            .map(|(stack_index, c)| {
+                lines
+                    .iter()
+                    .map(|line| line.chars().nth(stack_index).unwrap())
+                    .filter(|char| char.is_alphabetic())
+                    .collect()
+            })
+            .collect();
+        Ok(vec)
+    }
+}
+
+struct Data {
+    stack: Stack,
+    moves: Vec<Move>,
 }
 
 fn parse_moves(s: &str) -> Vec<Move> {
     Regex::new(r"(\d+).*(\d+).*(\d+)")
         .unwrap()
         .captures_iter(s)
-        .map(|cap| Move {
-            amount: cap[1].parse().unwrap(),
-            from: cap[2].parse().unwrap(),
-            to: cap[3].parse().unwrap(),
+        .map(|cap| {
+            Move(
+                cap[1].parse().unwrap(),
+                cap[2].parse().unwrap(),
+                cap[3].parse().unwrap(),
+            )
         })
         .collect()
 }
 
 #[aoc_generator(day5)]
-fn parse(input: &str) -> (Stack, Vec<Move>) {
+fn parse(input: &str) -> (Stacks, Vec<Move>) {
     let (stack_lines, moves_lines) = input.split("\n\n").collect_tuple().unwrap();
 
-    (parse_stack(stack_lines), parse_moves(moves_lines))
+    (
+        Stacks::from_str(stack_lines).unwrap(),
+        parse_moves(moves_lines),
+    )
 }
 
 #[aoc(day5, part1)]
-fn part_1((stacks, moves): &(Stack, Vec<Move>)) -> &str {
+fn part_1((stacks, moves): &(Stacks, Vec<Move>)) -> &str {
     ""
 }
 
 #[aoc(day5, part2)]
-fn part_2((stacks, moves): &(Stack, Vec<Move>)) -> &str {
+fn part_2((stacks, moves): &(Stacks, Vec<Move>)) -> &str {
     ""
 }
 
