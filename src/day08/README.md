bbe121c81d8e82e3ace00af3cb55794dbd88773e
Luke Storry <luke.storry@letsdothis.com>
2022-12-08 10:03:53 +0000

    day 8

---
+++ src/day08/README.md
@@ -0,0 +1,9 @@
+# 🎄 Advent of Code 2022 - day 8 🎄
+
+## Info
+
+Task description: [link](https://adventofcode.com/2022/day/8)
+
+## Notes
+
+I went down a gross enumerated indexing route which really messed me around for part 2, finally got it working but it was [really gross](https://github.com/LukeStorry/aoc2022/blob/be467c9a352dd965ec668f8e280b2647d95e4979/src/day08/__main__.py), had a coffee and then re-did it with slices.
--- src/day08/__main__.py
+++ src/day08/__main__.py
@@ -1,55 +1,42 @@
+from math import prod
+
 data = open("./src/day08/input.txt").read()
-# data = "30373\n25512\n65332\n33549\n35390"
 
 trees = [list(map(int, line)) for line in data.splitlines()]
 width, height = len(trees[0]), len(trees)
 
 
-def hidden(y, x):
-    left = any(1 for _x in range(x) if trees[y][_x] >= trees[y][x])
-    right = any(1 for _x in range(x + 1, width) if trees[y][_x] >= trees[y][x])
-    up = any(1 for _y in range(y) if trees[_y][x] >= trees[y][x])
-    down = any(1 for _y in range(y + 1, height) if trees[_y][x] >= trees[y][x])
-    return left and right and up and down
-
-
-print(sum(1 for x in range(width) for y in range(height) if not hidden(y, x)))
-
-
-def scenic(y, x):
-    left = next(
-        (
-            i + 1
-            for (i, _x) in enumerate(reversed(range(x)))
-            if trees[y][_x] >= trees[y][x]
-        ),
-        x,
+def slices(y, x):
+    return (
+        [trees[y][_x] for _x in reversed(range(x))],
+        [trees[y][_x] for _x in range(x + 1, width)],
+        [trees[_y][x] for _y in reversed(range(y))],
+        [trees[_y][x] for _y in range(y + 1, height)],
     )
-    right = next(
-        (
-            i + 1
-            for (i, _x) in enumerate(range(x + 1, width))
-            if trees[y][_x] >= trees[y][x]
-        ),
-        width - x - 1,
-    )
-    up = next(
-        (
-            i + 1
-            for (i, _y) in enumerate(reversed(range(y)))
-            if trees[_y][x] >= trees[y][x]
-        ),
-        y,
-    )
-    down = next(
-        (
-            i + 1
-            for (i, _y) in enumerate(range(y + 1, height))
-            if trees[_y][x] >= trees[y][x]
-        ),
-        height - y - 1,
-    )
-    return left * right * up * down
 
 
-print(max(scenic(y, x) for x in range(width) for y in range(height)))
+print(
+    sum(
+        1
+        for x in range(width)
+        for y in range(height)
+        if any(
+            not any(tree >= trees[y][x] for tree in direction)
+            for direction in slices(y, x)
+        )
+    )
+)
+
+print(
+    max(
+        prod(
+            next(
+                (i + 1 for (i, tree) in enumerate(direction) if tree >= trees[y][x]),
+                len(direction),
+            )
+            for direction in slices(y, x)
+        )
+        for x in range(width)
+        for y in range(height)
+    )
+)
