af73e6d23cf1953683d8413bb36beccdbea1077d
Luke Storry <luke.storry@letsdothis.com>
2022-12-09 18:27:54 +0000

    noew py template

--- src/template/__main__.py
+++ src/template/__main__.py
@@ -1,3 +1,24 @@
-from pathlib import Path
+from dataclasses import dataclass
+from unittest import TestCase
 
-data = open("./src/dayXX/input.txt").read()
+
+@dataclass
+class XYZ:
+    x: int
+    y: int
+
+    def one(self):
+        pass
+
+
+def solve(data: str):
+    lines = [x for lines in data.splitlines() for x in lines.split(" ")]
+    objs = [XYZ(0, 0) for _ in range(10)]
+    return int(data)
+
+
+TestCase().assertEqual(solve("0"), 0)
+TestCase().assertEqual(solve("1"), 1)
+TestCase().assertEqual(solve("1"), 2)
+
+# print(solve(open("./src/day1X/input.txt").read()))
