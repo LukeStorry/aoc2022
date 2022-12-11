76b244b51863ba555c1e1525bbb5708a42a078bf
Luke Storry <luke.storry@letsdothis.com>
2022-12-11 09:06:51 +0000

    day 11 part 2

---
+++ src/day11/README.md
@@ -0,0 +1,11 @@
+# 🎄 Advent of Code 2022 - day 11 🎄
+
+## Info
+
+Task description: [link](https://adventofcode.com/2022/day/11)
+
+## Notes
+More time trying to read the question than anything else, but a fun one for sure.
+
+Part 2 sent me down an optimisation route for a bit, trying to clean my code up and stop using 'eval' for the operation etc.
+Running it in the background as I was working on this, I realised it would take more than just some code optimisations, so I threw in a mod-lcm thing and it just worked, magic.
--- src/day11/__main__.py
+++ src/day11/__main__.py
@@ -1,42 +1,56 @@
 from dataclasses import dataclass
-from math import prod
+from math import lcm, prod
+import re
+from typing import Callable
 
 
 @dataclass
 class Monkey:
     test: int
     items: list
-    operation: str
+    operation: Callable
     test: int
-    true_recipient: int
-    false_recipient: int
+    true: int
+    false: int
     inspections: int = 0
 
     def __init__(self, input: str):
-        id, items, operation, test, true, false = input.splitlines()
-        self.id = int(id[-2])
-        self.items = [int(i) for i in items.split(":")[1].split(", ")]
-        self.operation = operation.split("= ")[1]
-        self.test = int(test.split(" ")[-1])
-        self.true_recipient = int(true[-1])
-        self.false_recipient = int(false[-1])
+        _, items, operation, test, true, false = input.splitlines()
+        self.items = [int(i) for i in re.findall(r"\d+", items)]
+        self.test = int(test.split("divisible by")[1])
+        self.true, self.false = int(true[-1]), int(false[-1])
 
-    def turn(self, monkeys):
+        match operation.split("= old ")[1].split():
+            case ["*", "old"]:
+                self.operation = lambda old: old * old
+            case ["*", num]:
+                self.operation = lambda old, num=num: old * int(num)
+            case ["+", num]:
+                self.operation = lambda old, num=num: old + int(num)
+
+    def turn(self, monkeys, lcm=None):
         while self.items:
             self.inspections += 1
-            old = self.items.pop(0)
-            worry = eval(self.operation) // 3
-            recipient = (
-                self.true_recipient if worry % self.test == 0 else self.false_recipient
-            )
+            worry = self.operation(self.items.pop(0))
+            if lcm:
+                worry = worry % lcm
+            else:
+                worry = worry // 3
+            recipient = self.true if worry % self.test == 0 else self.false
             monkeys[recipient].items.append(worry)
 
 
 data = open("./src/day11/input.txt").read()
-monkeys = [Monkey(s) for s in data.split("\n\n")]
 
-for _ in range(20):
+monkeys = [Monkey(s) for s in data.split("\n\n")]
+for round in range(20):
     for monkey in monkeys:
         monkey.turn(monkeys)
+print(prod(sorted(m.inspections for m in monkeys)[-2:]))
 
+monkeys = [Monkey(s) for s in data.split("\n\n")]
+lcm = lcm(*[m.test for m in monkeys])
+for round in range(10000):
+    for monkey in monkeys:
+        monkey.turn(monkeys, lcm)
 print(prod(sorted(m.inspections for m in monkeys)[-2:]))
