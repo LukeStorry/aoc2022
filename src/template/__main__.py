b80cf5ec1fbefc6f92538d2d74dd5a74ca8fe9d9
Luke Storry <luke.storry@letsdothis.com>
2022-12-11 18:12:09 +0000

    added more stuff to py template

--- src/template/__main__.py
+++ src/template/__main__.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
-from unittest import TestCase
+import re
+
+DIGITS = re.compile(r"\d+")
 
 
 @dataclass
@@ -7,18 +9,12 @@ class XYZ:
     x: int
     y: int
 
-    def one(self):
-        pass
+    def one(self, s):
+        d = int(DIGITS.findall(s)[0])
 
 
-def solve(data: str):
-    lines = [x for lines in data.splitlines() for x in lines.split(" ")]
-    objs = [XYZ(0, 0) for _ in range(10)]
-    return int(data)
+data = open("./src/day1x/input.txt").read()
 
 
-TestCase().assertEqual(solve("0"), 0)
-TestCase().assertEqual(solve("1"), 1)
-TestCase().assertEqual(solve("1"), 2)
-
-# print(solve(open("./src/day1X/input.txt").read()))
+lines = [x for lines in data.splitlines() for x in lines.split(" ")]
+objs = [XYZ(0, 0) for _ in range(10)]
