bb8d2e055a78cb4b8aa22fe831ee4da1d34449c9
Luke Storry <luke.storry@letsdothis.com>
2022-12-13 10:03:00 +0000

    fix typescript day 13

--- src/day13/index.ts
+++ src/day13/index.ts
@@ -1,34 +1,38 @@
 import run from "aocrunner";
 import _ from "lodash";
+const { isNumber, isArray, isNil } = _
 
-const parse = (input: string) =>
+type T = undefined[] | (number | T)[]
+
+const parse = (input: string): T[][] =>
   input.split('\n\n').map(pair => pair.split('\n').map(eval));
 
-const compare = (a, b, i): boolean | undefined => {
+const compare = (a: T, b: T, i = 0): boolean | undefined => {
   const left = a[i], right = b[i]
 
-  // end of list
-  if (_.isNil(left) && _.isNil(right)) return undefined // backtrack
-  if (_.isNil(left) && !_.isNil(right)) return true
-  if (!_.isNil(left) && _.isNil(right)) return false
+  if (isNil(left) || isNil(right)) {
+    if (!isNil(right)) return true
+    if (!isNil(left)) return false
+    return undefined // backtrack
+  }
+  if (left === right) return compare(a, b, i + 1)
+  if (isNumber(left) && isNumber(right)) return left < right
+  if (isArray(left) && isArray(right)) return compare(left, right) ?? compare(a, b, i + 1)
 
-  if (left == right) return compare(a, b, i + 1)
-  if (_.isNumber(left) && _.isNumber(right)) return left < right
-  if (_.isArray(left) && _.isArray(right)) return compare(left, right, 0) ?? compare(a, b, i + 1)
-
-  if (_.isArray(left)) return compare(left, [right], 0) ?? compare(a, b, i + 1)
-  if (_.isArray(right)) return compare([left], right, 0) ?? compare(a, b, i + 1)
+  if (isArray(left)) return compare(left, [right]) ?? compare(a, b, i + 1)
+  if (isArray(right)) return compare([left], right) ?? compare(a, b, i + 1)
   return false
 }
 
 
-const part1 = (input: string): number => parse(input).reduce((
-  (prev, [a, b], pair_index) => compare(a, b, 0) ? prev + (pair_index + 1) : prev), 0)
-// const part1 = (input: string) => parse(input).forEach(([a, b], pair_index) => {
-//   console.log(pair_index + 1, compare(a, b, 0));
-// })
+const part1 = (input: string): number => parse(input)
+  .reduce(((prev, [a, b], pair_index) => compare(a, b) ? prev + (pair_index + 1) : prev), 0)
 
-const part2 = (input: string) => parse(input);
+const new_pair: T[] = [[2], [[6]]]
+const part2 = (input: string) => parse(input)
+  .flatMap(p => p).concat(new_pair)
+  .sort(((a, b) => compare(a, b) ? -1 : 1))
+  .reduce((r, p, i) => new_pair.includes(p) ? r * (i + 1) : r, 1)
 
 run({
   part1: {
@@ -62,17 +66,37 @@ run({
     ],
     solution: part1,
   },
-  // part2: {
-  //   tests: [
-  //     {
-  //       input: `
+  part2: {
+    tests: [
+      {
+        input: `[1,1,3,1,1]
+[1,1,5,1,1]
 
-  //       `,
-  //       expected: 0,
-  //     },
-  //   ],
-  //   solution: part2,
-  // },
+[[1],[2,3,4]]
+[[1],4]
+
+[9]
+[[8,7,6]]
+
+[[4,4],4,4]
+[[4,4],4,4,4]
+
+[7,7,7,7]
+[7,7,7]
+
+[]
+[3]
+
+[[[]]]
+[[]]
+
+[1,[2,[3,[4,[5,6,7]]]],8,9]
+[1,[2,[3,[4,[5,6,0]]]],8,9]`,
+        expected: 140
+      },
+    ],
+    solution: part2,
+  },
   trimTestInputs: true,
   // onlyTests: true,
 });
