18f89c3113fb4c7c38812dae8b90d9734253900a
Luke Storry <luke.storry@letsdothis.com>
2022-12-14 11:06:56 +0000

    optimise day 14

--- src/day14/__main__.py
+++ src/day14/__main__.py
@@ -1,25 +1,23 @@
 import re
 
 data = "498,4 -> 498,6 -> 496,6\n503,4 -> 502,4 -> 502,9 -> 494,9"
-# data = open("./src/day14/input.txt").read()
+data = open("./src/day14/input.txt").read()
 
-rocks = set()
-for line in data.splitlines():
-    coords = [tuple(map(int, match)) for match in re.findall(r"(\d+),(\d+)", line)]
-    for (x1, y1), (x2, y2) in zip(coords, coords[1:]):
-        if x1 == x2:
-            frm, to = sorted((y1, y2))
-            rocks.update((x1, y) for y in range(frm, to + 1))
-        else:
-            frm, to = sorted((x1, x2))
-            rocks.update((x, y1) for x in range(frm, to + 1))
+blocked = set(
+    (x, y)
+    for line in data.splitlines()
+    if (coords := re.findall(r"(\d+),(\d+)", line))
+    for (x1, y1), (x2, y2) in zip(coords, coords[1:])
+    if (ys := sorted((int(y1), int(y2)))) and (xs := sorted((int(x1), int(x2))))
+    for y in range(ys[0], ys[1] + 1)
+    for x in range(xs[0], xs[1] + 1)
+)
 
-max_y = max(y for _, y in rocks) + 1
+max_y = max(y for _, y in blocked) + 1
 sands = set()
 part1 = None
 while True:
     x, y = (500, 0)
-    blocked = rocks.union(sands)
     while y < max_y:
         for option in ((x, y + 1), (x - 1, y + 1), (x + 1, y + 1)):
             if option not in blocked:
@@ -31,26 +29,19 @@ while True:
         part1 = part1 or len(sands)
 
     sands.add((x, y))
+    blocked.add((x, y))
 
     if y == 0:
         print(f"{part1}, {len(sands)}")
         break
 
-    # / DISPLAY
-    # min_x, max_x = (
-    #     min(_x for _x, _ in blocked),
-    #     max(_x for _x, _ in blocked),
-    # )
-    # print("\n" * 100)
-    # print(
-    #     "\n".join(
-    #         str(_y)
-    #         + "".join(
-    #             "#" if (_x, _y) in rocks else "o" if (_x, _y) in sands else "."
-    #             for _x in range(min_x, max_x + 1)
-    #         )
-    #         for _y in range(0, max_y + 1)
-    #     )
-    # )
-    # print()
-    # sleep(0.1)
+    print(
+        "\n".join(
+            f"{_y:03} "
+            + "".join(
+                "o" if (_x, _y) in sands else "#" if (_x, _y) in blocked else " "
+                for _x in range(330, 675)
+            )
+            for _y in range(0, max_y + 1)
+        )
+    )
