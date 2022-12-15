9938de27b13c37a2cda6699a7fef3424d3e92b4d
Luke Storry <luke.storry@letsdothis.com>
2022-12-15 15:42:11 +0000

    clean part 1

--- src/day15/__main__.py
+++ src/day15/__main__.py
@@ -1,30 +1,24 @@
 import re
 
 data = open("./src/day15/input.txt").read()
-
 sensors = [[int(x) for x in re.findall(r"-?\d+", line)] for line in data.splitlines()]
 
 # Part 1
-
 y = 2000000
 
-
-def cov(sensor_x, sensor_y, beacon_x, beacon_y):
-    distance = abs(beacon_x - sensor_x) + abs(beacon_y - sensor_y)
-    for x in range(sensor_x - distance, sensor_x + distance + 1):
-        if abs(x - sensor_x) + abs(y - sensor_y) <= distance:
-            yield x
-
-
-beacons = set(beacon_x for _, _, beacon_x, beacon_y in sensors if beacon_y == y)
-coverage = set(
-    x
-    for sensor_x, sensor_y, beacon_x, beacon_y in sensors
-    for x in cov(sensor_x, sensor_y, beacon_x, beacon_y)
+part1 = len(
+    set(
+        x
+        for sensor_x, sensor_y, beacon_x, beacon_y in sensors
+        if ((distance := abs(beacon_x - sensor_x) + abs(beacon_y - sensor_y)))
+        for x in range(sensor_x - distance, sensor_x + distance + 1)
+        if ((abs(x - sensor_x) + abs(y - sensor_y)) <= distance)
+    )
+    - set(beacon_x for _, _, beacon_x, beacon_y in sensors if beacon_y == y)
 )
-print(len(coverage - beacons))
+print(part1)
 
-# 5144286
+assert part1 == 5144286
 
 
 # Part 2
@@ -48,7 +42,8 @@ for y in range(0, max_coord):
 
     if len(ranges) == 1 or any(l <= 0 and r >= max_coord for l, r in ranges):
         continue
-    print((ranges[1][0] - 1) * 4000000 + y)
+    part2 = (ranges[1][0] - 1) * 4000000 + y
     break
 
-# 10229191267339
+print(part2)
+assert part2 == 10229191267339
