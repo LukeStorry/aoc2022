af6db94982a87e850dd064baac1a89f4bd8194bc
Luke Storry <luke.storry@letsdothis.com>
2022-12-17 18:59:30 +0000

    day 17 done

---
+++ src/day17/README.md
@@ -0,0 +1,11 @@
+# 🎄 Advent of Code 2022 - day 17 🎄
+
+## Info
+
+Task description: [link](https://adventofcode.com/2022/day/17)
+
+## Notes
+A tough but quite fun one I think.
+Original attempt got stuck in edge-cases of when rocks overlap, but I really didn't want a grid so doubled-down on the sets of coordinates with an array of max-heights, which worked well.
+
+Part 2 just being a really big number made me groan a bit. I tried a dumb naive attempt at finding some simple cycles, using the max-heights array I already had, not expecting there to be any (or for it to be correct), but then it worked! :shrug::smile:
--- src/day17/__main__.py
+++ src/day17/__main__.py
@@ -1,7 +1,7 @@
 data = list(open("./src/day17/input.txt").read())
-# data = list(">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>")
+data = list(">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>")
 
-rocks = [
+rock_shapes = [
     [(0, 0), (1, 0), (2, 0), (3, 0)],
     [(0, 1), (1, 0), (1, 1), (1, 2), (2, 1)],
     [(0, 0), (1, 0), (2, 0), (2, 1), (2, 2)],
@@ -9,18 +9,19 @@ rocks = [
     [(0, 0), (0, 1), (1, 0), (1, 1)],
 ]
 
-width, number_of_rocks, blows = 7, 0, 0
+width, n_rocks, blows = 7, 0, 0
 heights, taken_places = [0] * 7, set((x, 0) for x in range(width))
-
+cycles, extra_height = {}, 0
 blocked = lambda rock_type, x, y: any(
     x < 0 or x + dx >= width or (x + dx, y + dy) in taken_places
-    for dx, dy in rocks[rock_type]
+    for dx, dy in rock_shapes[rock_type]
 )
 
-while number_of_rocks < 2022:
+part1_rocks, part2_rocks = 2022, 1000000000000
+while n_rocks < part2_rocks:
     x, y = 2, max(heights) + 4
     while True:
-        rock_type = number_of_rocks % 5
+        rock_type = n_rocks % 5
         if data[blows] == "<" and not blocked(rock_type, x - 1, y):
             x -= 1
         if data[blows] == ">" and not blocked(rock_type, x + 1, y):
@@ -30,11 +31,24 @@ while number_of_rocks < 2022:
             break
         y -= 1
 
-    for dx, dy in rocks[rock_type]:
+    for dx, dy in rock_shapes[rock_type]:
         heights[x + dx] = max(heights[x + dx], y + dy)
         taken_places.add((x + dx, y + dy))
 
-    number_of_rocks += 1
+    n_rocks += 1
+    if n_rocks == 2022:
+        print(max(heights))
 
-print(max(heights))
-# 3068
+    normalised_heights = tuple(h - min(heights) for h in heights)
+
+    cycle = (rock_type, blows, normalised_heights)
+    if n_rocks > part1_rocks and cycle in cycles:
+        cycle_rocks, cycle_height = cycles[cycle]
+        repeats = ((part2_rocks - cycle_rocks) // (n_rocks - cycle_rocks)) - 1
+        extra_height += repeats * (max(heights) - cycle_height)
+        n_rocks += repeats * (n_rocks - cycle_rocks)
+
+    cycles[cycle] = (n_rocks, max(heights))
+
+
+print(max(heights) + extra_height)
