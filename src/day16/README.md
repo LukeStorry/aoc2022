2c006d54cd2517a96209f242aef24a98f0111749
Luke Storry <luke.storry@letsdothis.com>
2022-12-17 16:36:13 +0000

    day 16

---
+++ src/day16/README.md
@@ -0,0 +1,10 @@
+# 🎄 Advent of Code 2022 - day 16 🎄
+
+## Info
+
+Task description: [link](https://adventofcode.com/2022/day/16)
+
+## Notes
+
+Sheesh.
+Part1 was hard, tweaking it to part 2 was fun at first, but then the original solution took nearly an hour to run on the real data set, by which time I had optimised it and now it’s <1s
---
+++ src/day16/__main__.py
@@ -0,0 +1,72 @@
+import re
+from dataclasses import dataclass
+
+
+@dataclass
+class Valve:
+    name: str
+    flow_rate: int
+    leads_to: list[str]
+
+    def __hash__(self):
+        return hash(self.name)
+
+
+valves = [
+    Valve(name, int(rate), leads_to.split(", "))
+    for name, rate, leads_to in re.findall(
+        r"Valve ([A-Z]+) has flow rate=(\d+); tunnels? leads? to valves? ([A-Z, ]+)",
+        open("./src/day16/input.txt").read(),
+    )
+]
+
+distances = {
+    (v, to): 1 if to.name in v.leads_to else 999999 for to in valves for v in valves
+}
+
+for c in valves:
+    for a in valves:
+        for b in valves:
+            distances[a, b] = min(
+                distances[a, b],
+                distances[a, c] + distances[c, b],
+            )
+
+
+def try_paths(
+    current: Valve,
+    minute: int,
+    opened: frozenset[str],
+    pressure: int,
+    found_pressures: dict[frozenset[str], int],
+):
+    found_pressures[opened] = max(pressure, found_pressures.get(opened, 0))
+    for next in valves:
+        if next.flow_rate == 0:
+            continue
+        mins_left = minute - 1 - distances[current, next]
+        if next.name in opened or mins_left <= 0:
+            continue
+
+        try_paths(
+            next,
+            mins_left,
+            frozenset(opened.union([next.name])),
+            pressure + next.flow_rate * mins_left,
+            found_pressures,
+        )
+    return found_pressures
+
+
+AA = next(v for v in valves if v.name == "AA")
+print(max(try_paths(AA, 30, frozenset(), 0, {}).values()))
+
+part_2_run = try_paths(AA, 26, frozenset(), 0, {})
+print(
+    max(
+        my_pressure + elephant_pressure
+        for my_path, my_pressure in part_2_run.items()
+        for elephant_path, elephant_pressure in part_2_run.items()
+        if my_path.isdisjoint(elephant_path)
+    )
+)
