5ddf2ec6b48231b5bd984ae03e36dc16e7217c03
Luke Storry <luke.storry@letsdothis.com>
2022-12-18 09:20:07 +0000

    tidy day 18

--- src/day18/__main__.py
+++ src/day18/__main__.py
@@ -1,10 +1,18 @@
 import re
-from itertools import product
 
-cubes = set((int(x), int(y), int(z)) for x, y, z in re.findall(r"(\d+),(\d+),(\d+)", open("./src/day18/input.txt").read()))
+cubes = set(
+    (int(x), int(y), int(z))
+    for x, y, z in re.findall(
+        r"(\d+),(\d+),(\d+)", open("./src/day18/input.txt").read()
+    )
+)
 sides = ((1, 0, 0), (-1, 0, 0), (0, 1, 0), (0, -1, 0), (0, 0, 1), (0, 0, -1))
-surfaces = [(x + dx, y + dy, z + dz) for (x, y, z), (dx, dy, dz) in product(cubes, sides)]
-print(sum(1 for s in surfaces if s not in cubes))
+surfaces = [
+    c
+    for x, y, z in cubes
+    for c in ((x + dx, y + dy, z + dz) for dx, dy, dz in sides)
+]
+print(sum(1 for c in surfaces if c not in cubes))
 
 low, high = min((min(c) for c in cubes)) - 1, max((max(c) for c in cubes)) + 1
 stop, outside = False, set([(low, low, low)])
@@ -13,7 +21,11 @@ while not stop:
     for x, y, z in list(outside):
         for dx, dy, dz in sides:
             neighbor = (x + dx, y + dy, z + dz)
-            if neighbor not in cubes and neighbor not in outside and all(low <= n <= high for n in neighbor):
+            if (
+                neighbor not in cubes
+                and neighbor not in outside
+                and all(low <= n <= high for n in neighbor)
+            ):
                 outside.add(neighbor)
                 stop = False
 
