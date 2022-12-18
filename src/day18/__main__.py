1bffbd298bc8eb65f7aa974fc5d64eba0830e650
Luke Storry <luke.storry@letsdothis.com>
2022-12-18 11:38:51 +0000

    walrus break instead of stop bool to save a loc ;)

--- src/day18/__main__.py
+++ src/day18/__main__.py
@@ -1,3 +1,4 @@
+from itertools import product
 import re
 
 cubes = set(
@@ -8,25 +9,25 @@ cubes = set(
 )
 sides = ((1, 0, 0), (-1, 0, 0), (0, 1, 0), (0, -1, 0), (0, 0, 1), (0, 0, -1))
 surfaces = [
-    c
-    for x, y, z in cubes
-    for c in ((x + dx, y + dy, z + dz) for dx, dy, dz in sides)
+    (x + dx, y + dy, z + dz)
+    for (x, y, z), (dx, dy, dz) in product(cubes, sides)
 ]
 print(sum(1 for c in surfaces if c not in cubes))
 
 low, high = min((min(c) for c in cubes)) - 1, max((max(c) for c in cubes)) + 1
-stop, outside = False, set([(low, low, low)])
-while not stop:
-    stop = True
-    for x, y, z in list(outside):
-        for dx, dy, dz in sides:
-            neighbor = (x + dx, y + dy, z + dz)
-            if (
-                neighbor not in cubes
-                and neighbor not in outside
-                and all(low <= n <= high for n in neighbor)
-            ):
-                outside.add(neighbor)
-                stop = False
-
+outside = set([(low, low, low)])
+while True:
+    if next := set(
+        neighbor
+        for (x, y, z), (dx, dy, dz) in product(outside, sides)
+        if (
+            (neighbor := (x + dx, y + dy, z + dz))
+            and neighbor not in cubes
+            and neighbor not in outside
+            and all(low <= n <= high for n in neighbor)
+        )
+    ):
+        outside.update(next)
+    else:
+        break
 print(sum(1 for e in surfaces if e in outside))
