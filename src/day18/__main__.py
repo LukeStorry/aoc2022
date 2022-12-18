5ff58aa150996f59071910c863a34afe41035a06
Luke Storry <luke.storry@letsdothis.com>
2022-12-18 09:16:22 +0000

    move to using itertools product instead of set ops

--- src/day18/__main__.py
+++ src/day18/__main__.py
@@ -1,32 +1,20 @@
 import re
+from itertools import product
 
-cubes = set(
-    (int(x), int(y), int(z))
-    for x, y, z in re.findall(
-        r"(\d+),(\d+),(\d+)", open("./src/day18/input.txt").read()
-    )
-)
+cubes = set((int(x), int(y), int(z)) for x, y, z in re.findall(r"(\d+),(\d+),(\d+)", open("./src/day18/input.txt").read()))
 sides = ((1, 0, 0), (-1, 0, 0), (0, 1, 0), (0, -1, 0), (0, 0, 1), (0, 0, -1))
-edge_droplets = [
-    airs
-    for x, y, z in cubes
-    for airs in set((x + dx, y + dy, z + dz) for dx, dy, dz in sides) - cubes
-]
-print(len(edge_droplets))
+surfaces = [(x + dx, y + dy, z + dz) for (x, y, z), (dx, dy, dz) in product(cubes, sides)]
+print(sum(1 for s in surfaces if s not in cubes))
 
 low, high = min((min(c) for c in cubes)) - 1, max((max(c) for c in cubes)) + 1
-stop, outside = False, set([(low, low, low), (high, high, high)])
+stop, outside = False, set([(low, low, low)])
 while not stop:
     stop = True
     for x, y, z in list(outside):
         for dx, dy, dz in sides:
             neighbor = (x + dx, y + dy, z + dz)
-            if (
-                neighbor not in cubes
-                and neighbor not in outside
-                and all(low <= n <= high for n in neighbor)
-            ):
+            if neighbor not in cubes and neighbor not in outside and all(low <= n <= high for n in neighbor):
                 outside.add(neighbor)
                 stop = False
 
-print(sum(1 for e in edge_droplets if e in outside))
+print(sum(1 for e in surfaces if e in outside))
