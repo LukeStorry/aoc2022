68282cc426ea9f8a95134dd34752487d30897937
Luke Storry <luke.storry@letsdothis.com>
2022-12-21 11:05:15 +0000

    day 21 done

---
+++ src/day21/README.md
@@ -0,0 +1,10 @@
+# 🎄 Advent of Code 2022 - day 21 🎄
+
+## Info
+
+Task description: [link](https://adventofcode.com/2022/day/21)
+
+## Notes
+Didn't wanna write another proper parser, and it looked close enough for me to be able to run an `eval` with some simple string replacements.
+
+Worked a treat for p1, p2 was a bit of automated trial-and-error, but it solves it every other time in <1s, so...
--- src/day21/__main__.py
+++ src/day21/__main__.py
@@ -1,6 +1,28 @@
+from random import random
 import re
 
 input = open("./src/day21/input.txt").read()
 exec(re.sub(r"\(\)(\w+)", r"\1()", input.replace(": ", "=lambda:").replace(" ", "()")))
 print(int(root()))
 
+root_nodes = [x + "()" for x in re.findall(r"root: (\w+) . (\w+)", input)[0]]
+root = lambda: eval("==".join(root_nodes))
+
+estimates = []
+for _ in range(1000):
+    x1, x2 = random() * 10000, random() * 100000
+    humn = lambda: x1
+    y1 = eval(root_nodes[0]) - eval(root_nodes[1])
+    humn = lambda: x2
+    y2 = eval(root_nodes[0]) - eval(root_nodes[1])
+    estimates.append((x1 * y2 - x2 * y1) / (y2 - y1))
+
+avg = int(sum(estimates) / len(estimates))
+
+for x in range(avg - 10000, avg + 10000):
+    humn = lambda: x
+    if root():
+        print(x)
+        break
+
+# 3272260914328
