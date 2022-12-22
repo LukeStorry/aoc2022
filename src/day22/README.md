1e09f3cf8b6447f37c39b0f281b8e09b3bd89ac5
Luke Storry <luke.storry@letsdothis.com>
2022-12-22 18:12:01 +0000

    day 22 done

---
+++ src/day22/README.md
@@ -0,0 +1,10 @@
+# 🎄 Advent of Code 2022 - day 22 🎄
+
+## Info
+
+Task description: [link](https://adventofcode.com/2022/day/22)
+
+## Notes
+
+OK part 1, used complex numbers just for the extra fun.
+Part2's wrapping was a bit of a nightmare, I gave up being clever and instead hard-coded the 14 translations, bit meh but was glad to finally get it working.
--- src/day22/__main__.py
+++ src/day22/__main__.py
@@ -1,23 +1,74 @@
-from math import cos, pi, sin
 import re
 
 data = open("./src/day22/input.txt").read()
-tiles = {x + y * 1j for y, line in enumerate(data.splitlines()) for x, c in enumerate(line) if c == "."}
-rocks = {x + y * 1j for y, line in enumerate(data.splitlines()) for x, c in enumerate(line) if c == "#"}
-direction, location = 0, next(l for l in tiles if l.imag == 0)
-for distance, turn in re.findall(r"(\d+)([RL]?)", data.splitlines()[-1]):
-    step = int(cos(direction)) + 1j * int(sin(direction))
-    for _ in range(int(distance)):
-        next = location + step
-        if next in rocks:
-            break
-        if next not in tiles:
-            next -= 200 * step
-            while next not in tiles.union(rocks):
-                next += step
-        if next in rocks:
-            break
-        location = next
-    direction += pi / 2 if turn == "R" else -pi / 2
+tiles = {x + y * 1j for y, line in enumerate(data.splitlines(), 1) for x, c in enumerate(line, 1) if c == "."}
+rocks = {x + y * 1j for y, line in enumerate(data.splitlines(), 1) for x, c in enumerate(line, 1) if c == "#"}
+moves = re.findall(r"(\d+)([RL]?)", data.splitlines()[-1])
 
-print(int(location.imag + 1) * 1000 + int(location.real + 1) * 4 + [1, -1j, -1, 1j].index(step))
+
+def cube_wrapping(location, step):
+    x, y = (location).real, (location).imag
+    match step:
+        case 1:
+            if 1 <= y <= 50:
+                return 100 + 151j - 1j * y, -1
+            if 51 <= y <= 100:
+                return y + 50 + 50j, -1j
+            if 101 <= y <= 150:
+                return 150 + 151j - y * 1j, -1
+            if 151 <= y <= 200:
+                return y - 100 + 150j, -1j
+        case -1:
+            if 1 <= y <= 50:
+                return 151j - y * 1j + 1, 1
+            if 51 <= y <= 100:
+                return 101j + y - 50, 1j
+            if 101 <= y <= 150:
+                return 151j - y * 1j + 51, 1
+            if 151 <= y <= 200:
+                return 1j + y - 100, 1j
+        case -1j:
+            if 1 <= x <= 50:
+                return 50j + x * 1j + 51, 1
+            if 51 <= x <= 100:
+                return x * 1j + 100j + y, 1
+            if 101 <= x <= 150:
+                return 200j + x - 100, -1j
+        case 1j:
+            if 1 <= x <= 50:
+                return 1j + x + 100, 1j
+            if 51 <= x <= 100:
+                return x * 1j + 100j + 50, -1
+            if 101 <= x <= 150:
+                return x * 1j - 50j + 100, -1
+
+
+def solve(part1):
+    step, location = 1, next(l for l in tiles if l.imag == 1)
+    for distance, turn in moves:
+        for _ in range(int(distance)):
+            next_location = location + step
+            if next_location in rocks:
+                break
+            if next_location not in tiles:
+                if part1:
+                    next_location -= 200 * step
+                    while next_location not in tiles:
+                        next_location += step
+                    if next_location - step in rocks:
+                        break
+                else:
+                    next_location, next_step = cube_wrapping(next_location - step, step)
+                    if next_location not in rocks:
+                        step = next_step
+                    else:
+                        break
+
+            location = next_location
+
+        step *= 1j if turn == "R" else -1j if turn == "L" else 1
+    return int(location.imag) * 1000 + int(location.real) * 4 + [1, 1j, -1, -1j].index(step)
+
+
+print(solve(part1=True))
+print(solve(part1=False))
