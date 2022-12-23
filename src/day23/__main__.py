b5afdb09f565ff202f4bff98e92dc01838d56848
Luke Storry <luke.storry@letsdothis.com>
2022-12-23 11:27:42 +0000

    tidy  day23

--- src/day23/__main__.py
+++ src/day23/__main__.py
@@ -1,66 +1,47 @@
-from dataclasses import dataclass
-
-
-@dataclass(slots=True)
-class Elf:
-    location: complex
-    chosen_move: complex | None = None
-
-    def choose(self, other_locations: list[complex], offset: int):
-        self.chosen_move = None
-        surrounding = [self.location + d for d in ((-1 + 1j), 1, 1j, (1 + 1j), (-1 - 1j), (-0 - 1j), (1 - 1j), -1)]
-        if not (nearby := [l for l in other_locations if l in surrounding]):
-            return
-        for direction in ("N", "S", "W", "E")[offset:] + ("N", "S", "W", "E")[:offset]:
-            match direction:
-                case "N":
-                    if not any(self.location + d in nearby for d in (1j - 1, 1j, 1j + 1)):
-                        self.chosen_move = self.location + 1j
-                        return
-                case "S":
-                    if not any(self.location + d in nearby for d in (-1j - 1, -1j, -1j + 1)):
-                        self.chosen_move = self.location + -1j
-                        return
-                case "W":
-                    if not any(self.location + d in nearby for d in (-1 - 1j, -1, -1 + 1j)):
-                        self.chosen_move = self.location + -1
-                        return
-                case "E":
-                    if not any(self.location + d in nearby for d in (1 - 1j, 1, 1 + 1j)):
-                        self.chosen_move = self.location + 1
-                        return
-
-    def move(self, others: list["Elf"]):
-        assert self.chosen_move is not None
-        if self.chosen_move in (e.chosen_move for e in others if e != self):
-            return
-        self.location = self.chosen_move
-
+from collections import defaultdict
 
 data = open("./src/day23/input.txt").read()
-elves = [Elf(complex(x, y)) for y, line in enumerate(reversed(data.splitlines())) for x, c in enumerate(line) if c == "#"]
-
+elves = {(x, y) for y, line in enumerate(reversed(data.splitlines())) for x, c in enumerate(line) if c == "#"}
 
 for i in range(10000):
-    print(i)
-    locations = [e.location for e in elves]
-    for elf in elves:
-        elf.choose(locations, i % 4)
+    choices = defaultdict(list)
+    next_elves = set()
+    for x, y in elves:
+        directions = [
+            ((x, y + 1), ((x - 1, y + 1), (x, y + 1), (x + 1, y + 1))),  # North
+            ((x, y - 1), ((x - 1, y - 1), (x, y - 1), (x + 1, y - 1))),  # South
+            ((x - 1, y), ((x - 1, y - 1), (x - 1, y), (x - 1, y + 1))),  # West
+            ((x + 1, y), ((x + 1, y - 1), (x + 1, y), (x + 1, y + 1))),  # East
+        ]
+        if not any(check in elves for _, checks in directions for check in checks):
+            next_elves.add((x, y))
+            continue
+        for result, checks in directions[i % 4 :] + directions[: i % 4]:
+            if not any(e in elves for e in checks):
+                choices[result].append((x, y))
+                break
+        else:
+            next_elves.add((x, y))
 
-    elves_with_choices = [e for e in elves if e.chosen_move is not None]
-    if not elves_with_choices:
+    if not choices:
         break
-    for elf in elves_with_choices:
-        elf.move(elves_with_choices)
+
+    for choice, elves_with_choice in choices.items():
+        if len(elves_with_choice) == 1:
+            next_elves.add(choice)
+        else:
+            next_elves.update(elves_with_choice)
+
+    elves = next_elves
 
     if i == 9:
         print(
             "Part 1: ",
             sum(
                 1
-                for x in range(int(min(e.location.real for e in elves)), int(max(e.location.real for e in elves)) + 1)
-                for y in range(int(min(e.location.imag for e in elves)), int(max(e.location.imag for e in elves)) + 1)
-                if not any(complex(x, y) == e.location for e in elves)
+                for x in range(min(x for x, _ in elves), max(x for x, _ in elves) + 1)
+                for y in range(min(y for _, y in elves), max(y for _, y in elves) + 1)
+                if not any((x, y) == e for e in elves)
             ),
         )
 
